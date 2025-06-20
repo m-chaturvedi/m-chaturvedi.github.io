3cecc7709d232045bb81f9171a5615427e41b74c
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2025-06-20 11:36:09 -0400

    First commit for my homepage

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "",
+	url: "https://example.com/",
+	language: "en",
+	description: "Some articles and my resume.",
+	author: {
+		name: "Mmanu Chaturvedi",
+		email: "mmanu.chaturvedi@gmail.com",
+		url: "https://example.com/about-me/"
+	}
+}
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    content: ['./content/**/*.{njk,md}', './content/**/*.svg'],
+    plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  };
