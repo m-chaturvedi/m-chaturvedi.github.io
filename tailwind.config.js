81fe70f62a6a5ce014990d9489e5ffbbd54ed911
Mmanu Chaturvedi <mmanu.chaturvedi@gmail.com>
2025-06-22 05:07:40 -0400

    Add DaisyUI. Correct mobile display.  Correct publications.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     content: ['./content/**/*.{njk,md}', './content/**/*.svg'],
     plugins: [require('@tailwindcss/typography'), require('daisyui')],
+      daisyui: {
+        themes: ["light"], // Or just "light" if you only want light mode
+        // darkTheme: "light", // Optional: Explicitly sets the light theme as the default for dark mode
+      },
   };
